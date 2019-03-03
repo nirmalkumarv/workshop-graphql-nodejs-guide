58ad964ad248568b5e703b7b294c377c1a0a1a5f
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2019-03-03 06:30:14 +0100

    feat: copying from java guide

---
+++ _deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+git config --global user.email "ivan.corrales.solera@gmail.com"
+git config --global user.name "Iván Corrales Solera"
+
+rm -rf book-output
+git clone -b gh-pages https://github.com/wesovilabs-workshops/workshop-graphql-java-guide.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
